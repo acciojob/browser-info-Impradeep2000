b0ce2b0c950b35f39df73de9c32ec3b3c242bed8
Pradeep Kumar <95734296+Impradeep2000@users.noreply.github.com>
2023-08-27 13:23:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function displayBrowserInfo() {
+      const browserInfoElement = document.getElementById('browser-info');
+      const userAgent = navigator.userAgent;
+      const browserName = navigator.appName;
+      const browserVersion = navigator.appVersion;
+
+      const message = "You are using " + browserName + " version " + browserVersion;
+      browserInfoElement.textContent = message;
+}
+    displayBrowserInfo();
